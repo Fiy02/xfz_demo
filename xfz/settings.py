81fa97823fe8332a8dd7cca110161104b5e19b3c
Fiy <619066978@qq.com>
2019-07-05 21:26:06 +0800

    del libraries

--- xfz/settings.py
+++ xfz/settings.py
@@ -80,10 +80,6 @@ TEMPLATES = [
             'builtins': [
                 'django.templatetags.static'
             ],
-            'libraries':{
-                'news_filters': 'apps.news.templatetags.news_filters',
-                'payinfo_filters': 'apps.payinfo.templatetags.payinfo_filters'
-            }
         },
     },
 ]
