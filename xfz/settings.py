6712bf8079b8ee21aae8c5fea3eecdcd9860052d
Fiy <619066978@qq.com>
2019-07-05 17:31:59 +0800

    add static_root

--- xfz/settings.py
+++ xfz/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 
 # 静态文件存放路径；
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'front','static_dist')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'front','dist'),
 ]
