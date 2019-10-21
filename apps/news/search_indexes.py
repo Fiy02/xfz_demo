aeb8cf167a8889b0db9b4dea3ab63470bba43655
QM Li <619066978@qq.com>
2019-10-21 09:43:48 +0800

    edit search

--- apps/news/search_indexes.py
+++ apps/news/search_indexes.py
@@ -1,11 +1,15 @@
 from haystack import indexes
 from .models import News
 
+# 创建索引类，给 news 做索引所以在 news 文件下创建该类；
 class NewsIndex(indexes.SearchIndex,indexes.Indexable):
+    # 作为索引的主要字段；
     text = indexes.CharField(document=True,use_template=True)
 
+    # 指明该索引为哪个模型服务；
     def get_model(self):
         return News
 
+    # 索引时在模型中返回哪些值，这里返回所有数据；
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
